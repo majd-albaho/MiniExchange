c55425f32dcc43be9eaf273d1d6f0ae7af4ef380
Majd Albaho <majd.albaho@outlook.com>
2026-05-19 11:00:49 +0400

    Create AuthService

---
+++ AuthService/AuthService.Api/Grpc/AuthService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using AuthService.Api.Protos;
+
+namespace AuthService.Api.Grpc
+{
+    public class AuthService : TradingPairGrpc.TradingPairGrpcBase
+    {
+        public override Task<PingResponse> Ping(PingRequest request, ServerCallContext context) {
+            return Task.FromResult(new PingResponse { Message = $"Pong: {request.Message}" });
+        }
+    }
+}
