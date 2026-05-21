bb59c35c647b8286240baa8f91b3f34a05ddd8dd
Majd Albaho <majd.albaho@outlook.com>
2026-05-21 07:04:42 +0400

    Create wallet service

---
+++ WalletService/WalletService.Api/Grpc/WalletService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using WalletService.Api.Protos;
+
+namespace WalletService.Api.Grpc
+{
+    public class WalletService : TradingPairGrpc.TradingPairGrpcBase
+    {
+        public override Task<PingResponse> Ping(PingRequest request, ServerCallContext context) {
+            return Task.FromResult(new PingResponse { Message = $"Pong: {request.Message}" });
+        }
+    }
+}
---
+++ WalletService/WalletService.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGrpcService<WalletService.Api.Grpc.WalletService>();
+
+app.MapGet("/", () => $"Wallet Service is running version {typeof(Program).Assembly.GetName().Version}");
+
+app.Run();
