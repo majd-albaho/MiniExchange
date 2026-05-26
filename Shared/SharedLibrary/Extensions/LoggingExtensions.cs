350301743debfd83734b7786e5d4bb798cdb00fe
Majd Albaho <majd.albaho@outlook.com>
2026-05-26 09:52:41 +0400

    Add SerilogSupport in shared project

---
+++ Shared/SharedLibrary/Extensions/LoggingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SharedLibrary.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static IHostBuilder UseSharedLogger(this IHostBuilder hostBuilder) {
+            hostBuilder.UseSerilog((context, configuration) => {
+                configuration.Enrich.FromLogContext()
+                             .Enrich.WithEnvironmentName()
+                             .Enrich.WithProcessId()
+                             .Enrich.WithThreadId()
+                             .WriteTo.Console();
+            });
+
+            return hostBuilder;
+        }
+    }
+}
