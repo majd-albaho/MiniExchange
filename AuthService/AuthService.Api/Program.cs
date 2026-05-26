c2ea2ed18dd3386baa74e22c8e7eea5c044d5a72
Majd Albaho <majd.albaho@outlook.com>
2026-05-26 10:41:02 +0400

    Use local logger per service instead of shared one

--- AuthService/AuthService.Api/Program.cs
+++ AuthService/AuthService.Api/Program.cs
@@ -1,15 +1,22 @@
-using System.Text;
 using AuthService.Application;
 using AuthService.Infrastructure;
 using AuthService.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using SharedLibrary.Extensions;
+using Serilog;
+using Serilog.Context;
+using System.Diagnostics;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSharedLogger();
+builder.Host.UseSerilog((context, services, configuration) => {
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
 
 // Add services to the container.
 builder.Services.AddApplication();
@@ -22,10 +29,8 @@ builder.Services.AddGrpc();
 
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters {
             ValidateIssuer = true,
             ValidateAudience = true,
             ValidateLifetime = true,
@@ -39,16 +44,41 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
+using (var scope = app.Services.CreateScope()) {
     var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
     await db.Database.MigrateAsync();
 }
+
+
+app.Use(async (context, next) => {
+    var correlationId = context.Request.Headers["X-Correlation-ID"].FirstOrDefault() ?? context.TraceIdentifier;
+
+    context.Response.Headers["X-Correlation-ID"] = correlationId;
+
+    using (LogContext.PushProperty("CorrelationId", correlationId))
+    using (LogContext.PushProperty("TraceId", Activity.Current?.TraceId.ToString()))
+    using (LogContext.PushProperty("SpanId", Activity.Current?.SpanId.ToString()))
+    using (LogContext.PushProperty("RequestPath", context.Request.Path))
+    using (LogContext.PushProperty("RequestMethod", context.Request.Method)) {
+        await next();
+    }
+});
+
+app.UseSerilogRequestLogging(options => {
+    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) => {
+        diagnosticContext.Set("ClientIP", httpContext.Connection.RemoteIpAddress?.ToString());
+        diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+    };
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
@@ -56,6 +86,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
 app.MapGrpcService<AuthService.Api.Grpc.AuthService>();
 
 app.MapGet("/", () => $"Auth Service is running version {typeof(Program).Assembly.GetName().Version}");
--- WalletService/WalletService.Api/Program.cs
+++ WalletService/WalletService.Api/Program.cs
@@ -1,13 +1,19 @@
-using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
-using SharedLibrary.Extensions;
+using Serilog;
+using Serilog.Context;
+using System.Diagnostics;
 using WalletService.Application;
 using WalletService.Infrastructure;
 using WalletService.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSharedLogger();
+builder.Host.UseSerilog((context, services, configuration) => {
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
 
 // Add services to the container.
 builder.Services.AddApplication();
@@ -27,12 +33,33 @@ using (var scope = app.Services.CreateScope()) {
     await db.Database.MigrateAsync();
 }
 
+app.Use(async (context, next) => {
+    var correlationId = context.Request.Headers["X-Correlation-ID"].FirstOrDefault() ?? context.TraceIdentifier;
+
+    context.Response.Headers["X-Correlation-ID"] = correlationId;
+
+    using (LogContext.PushProperty("CorrelationId", correlationId))
+    using (LogContext.PushProperty("TraceId", Activity.Current?.TraceId.ToString()))
+    using (LogContext.PushProperty("SpanId", Activity.Current?.SpanId.ToString()))
+    using (LogContext.PushProperty("RequestPath", context.Request.Path))
+    using (LogContext.PushProperty("RequestMethod", context.Request.Method)) {
+        await next();
+    }
+});
+
+app.UseSerilogRequestLogging(options => {
+    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) => {
+        diagnosticContext.Set("ClientIP", httpContext.Connection.RemoteIpAddress?.ToString());
+        diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+    };
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
