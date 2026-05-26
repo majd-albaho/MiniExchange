0f2ecb47a68244f399f50f472591770495993693
Majd Albaho <majd.albaho@outlook.com>
2026-05-26 09:54:24 +0400

    Wallet service sql migration project

---
+++ WalletService/WalletService.SqlMigration/WalletDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using WalletService.Infrastructure.Persistence;
+
+namespace WalletService.SqlMigration
+{
+    public class WalletDbContextFactory : IDesignTimeDbContextFactory<WalletDbContext>
+    {
+        public WalletDbContext CreateDbContext(string[] args) {
+            var optionsBuilder = new DbContextOptionsBuilder<WalletDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MiniExchangeWallet;Trusted_Connection=True;TrustServerCertificate=True;",
+                sql => sql.MigrationsAssembly("WalletService.SqlMigration"));
+
+            return new WalletDbContext(optionsBuilder.Options);
+        }
+    }
+}
