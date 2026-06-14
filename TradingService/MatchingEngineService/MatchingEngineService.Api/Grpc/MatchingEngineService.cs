da7eea9e91aca1a7c875f94778c35ad6afb7364f
Majd Albaho <majd.albaho@outlook.com>
2026-06-14 19:44:56 +0400

    Initiate MatchineService project

---
+++ TradingService/MatchingEngineService/MatchingEngineService.Api/Grpc/MatchingEngineService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using MatchingEngineService.Api.Protos;
+
+namespace MatchingEngineService.Api.Grpc
+{
+    public class MatchingEngineService : TradingPairGrpc.TradingPairGrpcBase
+    {
+        public override Task<PingResponse> Ping(PingRequest request, ServerCallContext context) {
+            return Task.FromResult(new PingResponse { Message = $"Pong: {request.Message}" });
+        }
+    }
+}
